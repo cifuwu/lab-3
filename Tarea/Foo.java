678e0e40899bd06c3289522b0503289ffc0b08ef
NicolasVargasRM10 <pavezvargas@gmail.com>
2022-06-21 21:33:17 -0400

    Avances en encontrar palabras en cuadrante

--- Tarea/Foo.java
+++ Tarea/Foo.java
@@ -1,5 +1,7 @@
 package Tarea;
 
+import java.util.ArrayList;
+
 /*Esta clase devolverá el tiempo que se demora en trabajar. (EN NANOSEGUNDOS!)
 Atributos: Value -> Valor a guardar
 Metodo: getValue() -> Devuelve el valor de Value.
@@ -7,8 +9,40 @@ Metodo: getValue() -> Devuelve el valor de Value.
 
 public class Foo extends Thread {
 
+    /*  cuadrante[palabra1,
+                 palabra2,
+                 palabra3,
+                 palabra4 
+                 ]
+                 */
+    private ArrayList<String> cuadrante = new ArrayList<String>();
+    private String palabra;
+    private int pos;
+
     @Override
     public void run(){
-        
+        String line;
+        for (int i=0; i < cuadrante.size(); i++){
+            //En aux guardamos las lineas del cuadrante
+            line = cuadrante.get(i).toLowerCase();
+            //En el caso de que encuentra la palbra de forma no inversa
+            if (line.contains(palabra)){
+                pos = line.indexOf(palabra);
+            }else{
+
+            }
+        }
+    }
+
+    public void setCuadrante(ArrayList<String> cuadrante){
+        this.cuadrante = cuadrante;
+    }
+
+    public void setPalabra(String palabra){
+        this.palabra = palabra.toLowerCase();
+    }
+
+    public int getPos(){
+        return pos;
     }
  }
--- Tarea/Main.java
+++ Tarea/Main.java
@@ -3,11 +3,14 @@ package Tarea;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.JSpinner.NumberEditor;
+
 public class Main {
 	public static void main(String[] args) throws Exception {		
 		
 		Scanner entrada = new Scanner(System.in);
 		String palabra;
+		int size_palabra, numeroDeCuadrantes, size_sopaDeLetras;
 
 		ArrayList<String> sopaDeLetras = new ArrayList<String>();
 		Datos archivo = new Datos("C:\\Users\\pavez\\Desktop\\TareasSo\\Tarea 3\\T3\\Tarea\\hola.txt");
@@ -21,8 +24,27 @@ public class Main {
 		//Se pide la palabra por consola
 		System.out.print("Ingrese la plabra a buscar: ");
 		palabra = entrada.nextLine();
+		entrada.close();
+
+		//Calculamos el numero de caudrantes que nos dará el numero de hebras
+		size_palabra = palabra.length();
+		size_sopaDeLetras = sopaDeLetras.get(0).length();
+
+		numeroDeCuadrantes = cantidadDeCuadrantes(size_palabra, size_sopaDeLetras);
+
+		for (int i=0;  i < numeroDeCuadrantes; i++){
+			
+		}
 
 	
+	}
+
+	public static int cantidadDeCuadrantes(int size_palabra, int size_sopaDeLetras){
+		int mul1 = size_palabra * size_palabra;
+		int mult2 = size_sopaDeLetras* size_sopaDeLetras;
+		int cantidadDeCuadrantes = mult2/mul1;
+		return cantidadDeCuadrantes;
+
 	}
 
 }
