d23e2232dac4e42edd94cce8167e28bf4193b8c9
NicolasVargasRM10 <pavezvargas@gmail.com>
2022-06-21 20:55:29 -0400

    ultimos cambios

--- Tarea/Main.java
+++ Tarea/Main.java
@@ -1,10 +1,14 @@
 package Tarea;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws Exception {		
 		
+		Scanner entrada = new Scanner(System.in);
+		String palabra;
+
 		ArrayList<String> sopaDeLetras = new ArrayList<String>();
 		Datos archivo = new Datos("C:\\Users\\pavez\\Desktop\\TareasSo\\Tarea 3\\T3\\Tarea\\hola.txt");
 		archivo.Leer();
@@ -14,6 +18,11 @@ public class Main {
 			System.out.println(sopaDeLetras.get(i));
 		}
 
+		//Se pide la palabra por consola
+		System.out.print("Ingrese la plabra a buscar: ");
+		palabra = entrada.nextLine();
+
+	
 	}
 
 }
